7e89043234ebaa4c0b962950c26acfa2e9ab69e9
Vijai K. Sivakumar <vijai.sivakumar@solitontech.com>
2022-09-08 17:18:06 +0530

    Add dalete functionality

--- src/app.js
+++ src/app.js
@@ -2,19 +2,27 @@ const cafeList = document.getElementById('cafe-list');
 const form = document.getElementById('add-cafe-form');
 function renderCafe(docs) {
     //console.log(docs.data().name)
-    const cafe = document.createElement('li');
-    const names = document.createElement('span');
-    const city = document.createElement('span');
-     
-    //cafe.setAttribute('data-id',docs.id);
+    let cafe = document.createElement('li');
+    let names = document.createElement('span');
+    let city = document.createElement('span');
+    let cross = document.createElement('div')
+
+    cafe.setAttribute('data-id',docs.id);
     names.innerHTML = docs.data().name;
     city.innerHTML = docs.data().city;
+    cross.innerHTML = 'X';
 
     cafe.appendChild(names);
     cafe.appendChild(city);
-    
+    cafe.appendChild(cross);
+     
     cafeList.appendChild(cafe);
-
+    
+    cross.addEventListener('click', (e) => {
+        e.stopPropagation();
+        let id = e.target.parentElement.getAttribute('data-id');
+        db.collection('cafes').doc(id).delete();
+    })
 }
 db.collection('cafes').get().then((snapshot) => {
     snapshot.forEach(docs => {
